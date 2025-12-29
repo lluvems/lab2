3b59eed70429af76d1c4ad6fe7e9128d69642d04
Vera Mers <veramers@mail.ru>
2025-12-29 17:42:22 +0700

    Добавление класса ChessClock

---
+++ ChessClock.cpp
@@ -0,0 +1,74 @@
+#include "ChessClock.h"
+
+using namespace std;
+
+ChessClock::ChessClock(time_t timeLimit)
+    : whiteTimer(timeLimit)
+    , blackTimer(timeLimit)
+{
+}
+
+void ChessClock::startWhite() {
+    blackTimer.stop(); // останавливаем черных 
+    whiteTimer.start(); // запускаем белых
+}
+
+void ChessClock::startBlack() {
+    whiteTimer.stop(); // останавливаем белых
+    blackTimer.start(); // запускаем черных
+}
+
+// Переключить игрока
+void ChessClock::switchPlayer() {
+    if (whiteTimer.ON_OFF()) {
+        startBlack();
+    }
+    else if (blackTimer.ON_OFF()) {
+        startWhite();
+    }
+    else {
+        // Если никто не ходит, начинаем с белых
+        startWhite();
+    }
+}
+
+time_t ChessClock::getWhiteTime() const {
+    return whiteTimer.getRemainingTimeSec();
+}
+
+time_t ChessClock::getBlackTime() const {
+    return blackTimer.getRemainingTimeSec();
+}
+
+bool ChessClock::isTimeUp() const {
+    return whiteTimer.isTimeUp() || blackTimer.isTimeUp();
+}
+
+void ChessClock::reset() {
+    whiteTimer.reset();
+    blackTimer.reset();
+}
+
+void ChessClock::printTime() const {
+    time_t whiteSeconds = whiteTimer.getRemainingTimeSec();
+    time_t blackSeconds = blackTimer.getRemainingTimeSec();
+
+    int whiteMinutes = whiteSeconds / 60;
+    int whiteSecs = whiteSeconds % 60;
+
+    int blackMinutes = blackSeconds / 60;
+    int blackSecs = blackSeconds % 60;
+
+    cout << "=== Время игроков ===" << endl;
+    cout << "Белые: "
+        << (whiteMinutes < 10 ? "0" : "") << whiteMinutes << ":"
+        << (whiteSecs < 10 ? "0" : "") << whiteSecs;
+    if (whiteTimer.ON_OFF()) cout << " [ХОДИТ]";
+    cout << endl;
+
+    cout << "Черные: "
+        << (blackMinutes < 10 ? "0" : "") << blackMinutes << ":"
+        << (blackSecs < 10 ? "0" : "") << blackSecs;
+    if (blackTimer.ON_OFF()) cout << " [ХОДИТ]";
+    cout << endl;
+}
---
+++ ChessClock.h
@@ -0,0 +1,30 @@
+#ifndef CHESSCLOCK_H
+#define CHESSCLOCK_H
+
+#include "MoveTimer.h"
+
+class ChessClock {
+private:
+    MoveTimer whiteTimer; // таймер белых
+    MoveTimer blackTimer; // таймер черных
+
+public:
+    // Конструктор с лимитом времени (по умолчанию 5 минут)
+    ChessClock(time_t timeLimit = 300);
+
+    ChessClock(const ChessClock&) = delete;
+    ChessClock& operator=(const ChessClock&) = delete;
+
+    void startWhite(); // начать ход белых
+    void startBlack(); // начать ход черных
+    void switchPlayer(); // переключить игрока (остановить текущего, запустить следующего)
+
+    time_t getWhiteTime() const; // время белых в секундах
+    time_t getBlackTime() const; // время черных в секундах
+    bool isTimeUp() const; // проверить, не закончилось ли время у кого-то
+
+    void reset();
+    void printTime() const;
+};
+
+#endif
