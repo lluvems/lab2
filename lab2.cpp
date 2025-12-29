1d665268d2e27e1799ef2246769d0f3d1e601170
Vera Mers <veramers@mail.ru>
2025-12-29 19:31:39 +0700

    Добавление демонстрации работы классов

--- lab2.cpp
+++ lab2.cpp
@@ -1,8 +1,53 @@
-﻿// lab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include "timer.h"
+#include "ChessClock.h"
+#include "takenPieceController.h"
+#include "materialSituationController.h"
+#include <iostream>
+#include <chrono>
+#include <locale>
 
-int main()
-{
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    //Демонстрация таймера
+    Timer gameTimer;
+    std::cout << "Таймер запущен автоматически!" << std::endl;  
+    std::this_thread::sleep_for(std::chrono::seconds(2)); // Ждем 2 секунды
+    gameTimer.printTime();  
+    std::this_thread::sleep_for(std::chrono::seconds(4)); // Ждем еще 4 секунды
+    gameTimer.printTime();
+
+    //Демонстрация шахматных часоов
+    ChessClock clock(60); // 1 минута на игрока
+    clock.startWhite();
+    std::cout << "Начало игры:" << std::endl;
+    clock.printTime();    
+    std::this_thread::sleep_for(std::chrono::seconds(10)); // Ждем 10 секунд
+    clock.switchPlayer(); // Переключаем игрока
+    std::cout << "\nПосле 10 секунд:" << std::endl;
+    clock.printTime();
+    if (clock.isTimeUp()) {
+        std::cout << "\nВремя вышло!" << std::endl;
+    }
+
+    //Демонстрация контроллера взятых фигур
+    TakenPieceController controller;
+    std::cout << "Начало игры:" << std::endl;
+    controller.printTakenPieces();
+    controller.cutQueen(color::white);   // черные взяли белого ферзя
+    controller.cutPawn(color::white);    // черные взяли белую пешку
+    controller.cutPawn(color::white);    // черные взяли еще одну белую пешку
+    controller.cutRook(color::black);    // белые взяли черную ладью
+    controller.cutKnight(color::black);  // белые взяли черного коня
+    std::cout << "\nПосле нескольких ходов:" << std::endl;
+    controller.printTakenPieces();
+
+    //Демонстрация Контроллера метермльного преимущества
+    TakenPieceController takenController;
+    MaterialSituationController materialController(&takenController);
+    takenController.cutQueen(color::white);   // черные взяли белого ферзя
+    takenController.cutPawn(color::white);    // черные взяли белую пешку
+    takenController.cutRook(color::black);    // белые взяли черную ладью
+    materialController.printMaterialSituation();
 
 }
-
